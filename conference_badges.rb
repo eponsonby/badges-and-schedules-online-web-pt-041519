e792e35872eb3de73ad947920a88a0b1609d5479
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 12:05:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,12 @@
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array_of_names)
+  array_of_names.collect do |name|
+    badge_maker
+  end
+end
+    
