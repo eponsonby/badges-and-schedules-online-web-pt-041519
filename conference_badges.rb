7bb72a643bfa115aabdcef77312e110e0eb895f8
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 12:11:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,4 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-rooms = [1, 2, 3, 4, 5, 6, 7]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
@@ -12,4 +11,9 @@ def batch_badge_creator(list_of_speakers)
 end
 
 def assign_rooms(list_of_speakers)
-    
+  room_number = 1
+  list_of_speakers.collect do |name|
+    "Hello, #{name}! You'll be assigned to room #{room_number}"
+    rooms_number += 1
+  end
+end
